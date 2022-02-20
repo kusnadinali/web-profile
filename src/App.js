7dde5081a61f834442b4aa8953f72dbc2c18a00d
kusnadinali <muhammad.ali.tif20@polban.ac.id>
2022-02-20 15:55:51 +0700

    web profil FE 80%

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SideNavbar from './Component/SideNavbar/SideNavbar';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+import DafMaha from './Component/Home/Home';
+import PostTeam from './Component/PostTeam/PostTeam';
+import AllTeam from './Component/AllTeam/AllTeam';
+import PutTeam from './Component/PutTeam/PutTeam';
+import DelTeam from './Component/DelTeam/DelTeam';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <SideNavbar />
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/reports' element={<Reports/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/team' element={<DafMaha/>} />
+          <Route path='/addteam' element={<PostTeam/>} />
+          <Route path='/allteam' element={<AllTeam/>} />
+          <Route path='/putteam' element={<PutTeam/>} />
+          <Route path='/delteam' element={<DelTeam/>} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
